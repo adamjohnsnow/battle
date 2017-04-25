98dc218df15d182ed7f88bf0b5c545ae622eb7a8
Adam Snow <adamjohnsnow@icloud.com>
2017-04-25 21:19:30 +0100

    removed random file

--- app.rb
+++ app.rb
@@ -10,13 +10,17 @@ class Battle < Sinatra::Base
 
   post '/names' do
     session[:player_one] = params[:player_one]
-    session[:player_two] = params['player_two']
+    session[:player_two] = params[:player_two]
+    session[:p1_hp] = 60
+    session[:p2_hp] = 60
     redirect '/play'
   end
 
   get '/play' do
     @player_one = session[:player_one]
+    @p1_hp = session[:p1_hp]
     @player_two = session[:player_two]
+    @p2_hp = session[:p2_hp]
     erb(:play)
   end
 
--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -2,9 +2,16 @@ feature 'Testing infrastructure' do
 
   scenario 'entering names' do
     visit('/')
-    fill_in('player_one', with: 'Jeremy')
-    fill_in('player_two', with: 'Adam')
+    fill_in('player_one', with: 'The Undertaker')
+    fill_in('player_two', with: 'Daniel Bryan')
     click_button('Submit')
-    expect(page).to have_content('Jeremy vs Adam for the championship!')
+    expect(page).to have_content('The Undertaker vs Daniel Bryan for the World Heavyweight Championship!')
+  end
+  scenario 'Have HP' do
+    visit('/')
+    fill_in('player_one', with: 'The Undertaker')
+    fill_in('player_two', with: 'Daniel Bryan')
+    click_button('Submit')
+    expect(page).to have_content('The Undertaker: 60HP')
   end
 end
